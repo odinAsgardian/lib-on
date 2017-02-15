51b3ef2faaeaa248db7d45613980413cad45bcfe
odinAsgardian <odinmiguel97@gmail.com>
2017-02-15 12:16:56 -0300

    Arquivos do beta

---
+++ index.php
@@ -0,0 +1,64 @@
+<html>
+	<head>
+		<title>Lib-On</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/lib.css">
+		<script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	</head>
+	<body>
+		<div class="container-fluid">
+			<div class="row content">
+				<div class="col-sm-3 sidenav">
+					<ul class="nav nav-pills nav-stacked">	
+						<div class="row">
+					        <div class="col-md-1 text-center">
+					          <img src="img/genos-one-punch-man-ova-29.4.jpg" class="img-circle" height="65" width="65" alt="Avatar">
+					        </div>
+					       </div><br>
+						<li class="active"><a href="#">Home</a></li>
+						<li><a href="#">Aluguel</a></li>
+						<li><a href="#">Renovação</a></li>
+						<li><a href="#">Cadastro</a></li>
+						<li><a href="#">Tabela de Alugados</a></li>
+					</ul><br>
+						<div class="input-group">
+							<input type="text" class="form-control" placeholder="Procure">
+							<span class="input-group-btn">
+								<button class="btn btn-default" type="button">
+									<span class="glyphicon glyphicon-search"></span>
+								</button>
+							</span>
+						</div>
+				</div>
+
+				<div class="col-sm-9">
+					<h4><small>TAREFAS</small></h4>
+					<hr>
+					<h2>I LOVE BOOKS</h2>
+					<h5><span class="label label-danger">Atrasado</span> <span class="label label-primary"> Renovação</span></h5><br>
+					<h5><span class="glyphicon glyphicon-time"></span> Post by Raissa, Feb 15, 2017</h5>
+						<p>Book is my passion. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><br><br>
+						<h4><small>RECENTS POSTS</small></h4>
+						<hr>
+						<h5><span class="glyphicon glyphicon-time"></span> Post by "Nome_do_usuário", "data", "ano"</h5>
+						<h5><span class="label label-success">Aluguel</span></h5>
+						<p>Book is my passion. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				<h4>Leave a Comment:</h4>
+			      <form role="form">
+				        <div class="form-group">
+					        <textarea class="form-control" rows="3" required></textarea>
+				        </div>
+					    <button type="submit" class="btn btn-success">Submit</button>
+					 </form>
+				</div>
+
+
+			</div>
+		</div>
+
+		 
+	</body>
+</html>
